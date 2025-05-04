326753ce51c6c55f8daf72dd81311d8422f2b784
DivinityQQ <DivinityQQ@gmail.com>
2025-05-04 14:25:37 +0200

    Add Pi Pico SMPS PWM mode setup to the rest of the benchmarks

--- src/keyword_spotting/keyword_spotting_benchmark.cpp
+++ src/keyword_spotting/keyword_spotting_benchmark.cpp
@@ -209,6 +209,22 @@ void keyword_spotting_setup() {
   // at the cost of higher idle power consumption
   digitalWrite(32+1, HIGH);
   #endif
+  
+  #ifdef CONFIG_IDF_TARGET_ESP32C6
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);
+  #endif
+
+  #ifdef ARDUINO_RASPBERRY_PI_PICO_W
+  // digitalWrite(PIN_LED, HIGH);
+
+  // Put the SMPS into PWM mode for better power consumption readibility
+  // at the cost of higher idle power consumption
+  digitalWrite(32+1, HIGH);
+  #endif
 
   // Set up logging. Google style is to avoid globals or statics because of
   // lifetime uncertainty, but since this has a trivial destructor it's okay.
